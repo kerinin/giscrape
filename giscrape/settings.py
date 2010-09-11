1aaadd5008d1c59dcd72a65d3ebc85353a9e6ae7
Ryan Michael <kerinin@gmail.com>
2010-09-11 12:46:26 -0500

    initial import

---
+++ giscrape/settings.py
@@ -0,0 +1,20 @@
+# Scrapy settings for giscrape project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+# Or you can copy and paste them from where they're defined in Scrapy:
+# 
+#     scrapy/conf/default_settings.py
+#
+
+BOT_NAME = 'giscrape'
+BOT_VERSION = '1.0'
+
+SPIDER_MODULES = ['giscrape.spiders']
+NEWSPIDER_MODULE = 'giscrape.spiders'
+DEFAULT_ITEM_CLASS = 'giscrape.items.GiscrapeItem'
+USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+
